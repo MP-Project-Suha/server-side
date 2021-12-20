07ef34f55e27a4309dcf00aca31712ddb98cec7f
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-12-20 14:05:32 +0300

    event schema

---
+++ db/models/event.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    shortDisc: { type: String, required: true },
+    longDisc: { type: String },
+    images: { type: Array, required: true },
+    location: { type: String, required: true },
+    cost: { type:Number, required: true },
+    createdAt: { type: Date, default: Date.now },
+    biginAt: { type: Date, required: true },
+    endAt: { type: Date, required: true },
+    isDele: { type: Boolean, default: false, required: true },
+    isVerfied: { type: Boolean, default: false, required: true },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Event", eventSchema);
