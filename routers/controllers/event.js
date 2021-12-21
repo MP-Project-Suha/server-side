41dbe74caf440bb3eb99fa657b1c3cbcc6753f68
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-12-21 09:36:29 +0300

    get my events and single event

--- routers/controllers/event.js
+++ routers/controllers/event.js
@@ -34,7 +34,57 @@ const addEvent = (req, res) => {
     }
   };
 
+  const getEvent = (req, res) => {
+    const { _id } = req.params;
+    const userId = req.suha._id;
+  
+    userModel
+      .findOne({ _id: userId ,isDele:true})
+      .then((user) => {
+        if (user) {
+    
+            eventModel
+              .findOne({ _id, isDele: false })
+              .populate("createdBy")
+              .then((result) => {
+                if (result) {
+                  res.status(200).json(result);
+                } else {
+                  res.status(404).json("There is no event for you");
+                }
+              })
+              .catch((err) => {
+                res.status(400).json(err);
+              });
+          
+        } else {
+          res.status(404).json("user dose not exist");
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
+
+  //get all event (not deleted)
+const allEvents = (req, res) => {
+    eventModel
+      .find({ isDele: false })
+      .populate("createdBy")
+      .then((result) => {
+        if (result) {
+          res.status(201).json(result);
+        } else {
+          res.status(404).json("There is no events to show");
+        }
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  };
   module.exports = {
     addEvent,
+    getEvent,
+    allEvents,
 
   };
--- routers/routes/event.js
+++ routers/routes/event.js
@@ -9,11 +9,13 @@ const authorization = require("../auth/authorization");
 // Destructuring controllers
 const {
   addEvent,
-
+  getEvent,
+  allEvents,
 } = require("../controllers/event");
 
 // Add new event 
 eventRouter.post("/myEvent",authentication, addEvent);
-
+eventRouter.get("/myEvent/:_id",authentication, getEvent);
+eventRouter.get("/myEvents",authentication, allEvents);
 
 module.exports = eventRouter;
